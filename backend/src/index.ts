8b776ba26260ff1f95eadb2da865875bc3f4ea89
Zynalex9 <alzn952@gmail.com>
2025-01-25 20:01:50 +0500

    Socket.io setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
-
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 const app = express();
+app.use(cors());
 const PORT = 3000;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user is connected", socket.id);
+  socket.on("disconnect",()=>{
+    console.log("User is disconnected")
+  })
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhosts:${PORT}`);
 });
